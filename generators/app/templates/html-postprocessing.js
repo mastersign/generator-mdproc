9e7f33abe86dbfd443834097f12ef9c5a8a83069
Tobias Kiertscher <kiertscher@th-brandenburg.de>
2017-12-05 15:30:00 +0100

    improve livereload support by preventing multiple reloads in case multiple files change

--- generators/app/templates/html-postprocessing.js
+++ generators/app/templates/html-postprocessing.js
@@ -8,10 +8,23 @@ module.exports = function (htmltext, opts) {
 	 * inject live-reload code
 	 */
     if (opts.cfg.injectLiveReload) {
-    	var liveReloadSnippet = "<script>document.write('<script src=\"http://'" +
+    	var liveReloadSnippet = "<script>" +
+            "document.write('<script src=\"http://'" +
         	"+ (location.host || 'localhost').split(':')[0]" +
         	"+ ':" + opts.cfg.livereload_port + "/livereload.js?snipver=1\"></'" +
-        	"+ 'script>')</script>"
+        	"+ 'script>');" +
+            // inject livereload plugin for filtering changes by filename
+            // to prevent multiple page reloads when multiple output files change
+            "window.onload = function () {" +
+            "  var filterPlugin = { reload: function (path) { " +
+            "    var pathParts = path.split(/\\/|\\\\/g);" +
+            "    var locationParts = window.location.pathname.split('/');" +
+            "    console.log(pathParts[pathParts.length-1] + ' =? ' + locationParts[locationParts.length - 1]);" +
+            "    return pathParts[pathParts.length-1] != locationParts[locationParts.length - 1];" +
+            "  }};" +
+            "  window.LiveReload.reloader.addPlugin(filterPlugin);" +
+            "};" +
+            "</script>";
         htmltext = htmltext.replace("</body>", liveReloadSnippet + "\n</body>");
     }
 
