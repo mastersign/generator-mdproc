fda81993235ec964ea8d302ac93192ffba1bd21a
Tobias Kiertscher <kiertscher@th-brandenburg.de>
2018-11-13 09:49:39 +0000

    extended automated tests for actual installing dependencies and executing the gulp steps

--- test/test-app.js
+++ test/test-app.js
@@ -4,11 +4,34 @@
 var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
+var cmdExists = require('command-exists').sync;
+var childproc = require('child_process');
+
 
 describe('mdproc:app', function () {
 	describe('Minimal', function () {
+
+		var tmpDirPath = null;
+
+		function runCliTool(cmd, cb) {
+			var proc = childproc.exec(cmd, { cwd: tmpDirPath });
+			var tool = cmd.match(/^\w+/)[0];
+			proc.on('error', cb);
+			proc.on('exit', function (exitCode) {
+				if (exitCode !== 0) {
+					cb(new Error('Command ' + tool + ' exited with status code ' + exitCode));
+				} else {
+					cb();
+				}
+			});
+		}
+
 		before(function (done) {
 			helpers.run(path.join(__dirname, '../generators/app'))
+				.inTmpDir(function (dir) {
+					tmpDirPath = dir;
+					// console.log('      working dir: ' + dir);
+				})
 				.withOptions({ skipInstall: true, skipEnd: true })
 				.withPrompts({
 					projectType: 'Minimal',
@@ -36,6 +59,64 @@ describe('mdproc:app', function () {
 				'test-project-name.sublime-project',
 				'src/index.md'
 			]);
+		});
+
+		describe('build', function () {
+
+			before('installing dependencies', function (done) {
+				this.timeout(120000);
+				if (cmdExists('yarn')) {
+					runCliTool('yarn install', done);
+				} else if (cmdExists('npm')) {
+					runCliTool('npm install', done);
+				} else {
+					done();
+				}
+			});
+
+			describe('HTML', function () {
+				before ('running gulp', function (done) {
+					this.timeout(30000);
+					if (cmdExists('pandoc')) {
+						runCliTool('yarn gulp html', done);
+					} else {
+						done();
+					}
+				});
+
+				it('should have build the HTML output', function () {
+					if (!(cmdExists('yarn') || cmdExists('npm')) ||
+						!cmdExists('pandoc')) {
+						this.skip();
+						return;
+					}
+					assert.file([
+						'out/index.html'
+					]);
+				});
+			});
+
+			describe('DOCX', function () {
+				before ('running gulp', function (done) {
+					this.timeout(30000);
+					if (cmdExists('pandoc')) {
+						runCliTool('yarn gulp docx', done);
+					} else {
+						done();
+					}
+				});
+
+				it('should have build the DOCX output', function () {
+					if (!(cmdExists('yarn') || cmdExists('npm')) ||
+					    !cmdExists('pandoc')) {
+						this.skip();
+						return;
+					}
+					assert.file([
+						'out/index.docx'
+					]);
+				});
+			});
 		});
 	});
 });
